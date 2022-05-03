1f076d88bd68316e24cc1250b96f72799e02b18c
Ben Johnson <benjohnson@Bens-MacBook-Pro-2.local>
2022-05-02 18:27:36 -0700

    feat: update

--- db.tf
+++ db.tf
@@ -4,17 +4,17 @@ module "db" {
   identifier = "demodb"
 
   engine            = "postgres"
-  engine_version    = "5.7.25"
+  engine_version    = "12"
   instance_class    = "db.t3.medium"
   allocated_storage = 5
 
   db_name  = "demodb"
-  username = "user"
+  username = "retool"
   port     = "3306"
 
   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
 
   maintenance_window = "Mon:00:00-Mon:03:00"
   backup_window      = "03:00-06:00"
@@ -26,7 +26,7 @@ module "db" {
 
   # DB subnet group
   create_db_subnet_group = true
-  subnet_ids             = [module.vpc.vpc_id]
+  subnet_ids             = [data.aws_subnet.a.id, data.aws_subnet.b.id, data.aws_subnet.c.id]
 
   # Database Deletion Protection
   deletion_protection = true
--- ec2.tf
+++ ec2.tf
@@ -4,15 +4,16 @@ module "ec2_instance" {
 
   name = "single-instance"
 
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
+  ami                    = "ami-0cb4e786f15603b0d"
+  instance_type          = "t3.micro"
+  key_name               = "retool-key"
   monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = data.aws_subnet.a.id
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
 }
+
